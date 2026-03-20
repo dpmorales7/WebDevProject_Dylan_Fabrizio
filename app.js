fdc61f0a0c2d87c0e22e628d05039d98f5d312fd
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2026-03-20 18:56:40 -0400

    Initialize Express server in app.js
    Initial setup for Node.js Express server with static file serving.

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js  –  DevPortfolio Hub  –  Node.js Express Server
+// CS 456 Web Programming – Assignment #3
+ 
+const express = require('express');
+const path = require('path');
+ 
+//const projectCtrl = require('./controller/ProjectController');
+ 
+const app = express();
+const PORT = process.env.PORT || 3000;
+ 
+//  mid ware
+app.use(express.json());
+ 
+// static server
+// Serve everything in the public_html as static files
+app.use(express.static(path.join(__dirname, 'public_html')));
+ 
+// rest api
+//app.get('/api/projects',     projectCtrl.getAll);
+//app.get('/api/projects/:id', projectCtrl.getOne);
+ 
+// start server
+app.listen(PORT, () => {
+  console.log(`DevPortfolio Hub running at http://localhost:${PORT}`);
+});
