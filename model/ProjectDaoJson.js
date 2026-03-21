2ff60e4ff325c5eaf3f4e37d487aef6b76d1a266
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2026-03-20 22:33:40 -0400

    Create ProjectDaoJson.js for project data access

---
+++ model/ProjectDaoJson.js
@@ -0,0 +1,19 @@
+// ProjectDaoJson.js
+// Dao for projects
+ 
+const fs = require('fs');
+const path = require('path');
+ 
+const DATA_FILE = path.join(__dirname, 'projects.json');
+ 
+// load projects from json
+let lstProjects = JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
+ 
+exports.readAll = function () {
+  return lstProjects;
+};
+ 
+exports.read = function (id) {
+  return lstProjects.find(p => p._id === id) || null;
+};
+ 
