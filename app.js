cb3f9dd7da8e56c0435775a907f8ef8240679073
dylanmorales <dpmorales@loyola.edu>
2026-04-18 00:34:31 -0400

    Made changes so it will use the new dao and controller to fetch api info from data base

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+
+require('dotenv').config();
+// app.js  –  DevPortfolio Hub  –  Node.js Express Server
+// CS 456 Web Programming – Assignment #5
+
+const express = require('express');
+const path = require('path');
+
+const projectCtrl = require('./controller/ProjectControllerMongo');
+
+const app  = express();
+const PORT = process.env.PORT || 4000;
+
+//  midware
+app.use(express.json());
+
+// server
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+// rest api for projects
+app.get   ('/api/projects',      projectCtrl.getAll);
+app.get   ('/api/projects/:id',  projectCtrl.getOne);
+app.post  ('/api/projects',      projectCtrl.create);
+app.put   ('/api/projects/:id',  projectCtrl.update);
+app.delete('/api/projects/:id',  projectCtrl.delete);
+
+// error handling
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+// server start
+app.listen(PORT, () => {
+  console.log(`DevPortfolio Hub running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
+
+
+//old app below
+
+/*
 // app.js  –  DevPortfolio Hub  –  Node.js Express Server
 // CS 456 Web Programming – Assignment #3
  
@@ -28,3 +71,4 @@ app.delete('/api/projects/:id', projectCtrl.delete);
 app.listen(PORT, () => {
   console.log(`DevPortfolio Hub running at http://localhost:${PORT}`);
 });
+*/
